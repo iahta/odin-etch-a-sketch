ba320cbb4a6fdf514cf2ab3e1fa1b9b3c86a0665
iahta <153889016+iahta@users.noreply.github.com>
2024-02-15 22:53:49 -0600

    added button, remove and replace grid with user prompt

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
 
 const container = document.getElementById('grid-container');
+const promptButton = document.getElementById('grid-prompt'); 
 let gridSize = (16 * 16);
+let userNumber = 16; 
 
+createSquares(gridSize,userNumber);
 
-
-
-function createSquares(gridSize) {
+function createSquares(gridSize,userNumber) {
     for (i = 0; i < gridSize; i++ ) {
         const gridSquares = document.createElement('div');
         container.appendChild(gridSquares); 
         gridSquares.classList.add('square');
-        gridSquares.textContent = i; 
+        let squareSize = (960 / userNumber);
+        gridSquares.style.width = squareSize + "px";
+        gridSquares.style.height = squareSize + "px"; 
         gridSquares.addEventListener('mouseover', () => {
             gridSquares.style.backgroundColor = 'red'; 
         } );
     }
     
-   
-}
+};
 
-createSquares(gridSize);
+promptButton.addEventListener('click', () => {
+    removeSquares(); 
+    let userNumber = Number(window.prompt("Pick A Number 100 And Below:"));
+if (userNumber > 100) {
+    userNumber = Number(window.prompt("Number Must Be 100 Or Less!"));
+}
+    let gridSize = (userNumber * userNumber); 
+    createSquares(gridSize, userNumber); 
+});
+
+function removeSquares() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+};
